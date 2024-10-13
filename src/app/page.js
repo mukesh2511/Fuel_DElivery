3d4991c2f49daff8e11e2fd6b244eb85c46d6eda
mukesh2511 <mouryamukesh8692@gmail.com>
2024-10-13 19:09:00 +0530

    another

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/login");
+  }, []);
   return (
     <div className={styles.page}>
       <main className={styles.main}>
